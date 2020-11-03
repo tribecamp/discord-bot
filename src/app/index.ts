93b7112ba72e77fd35b5515994d5675252e31ba1
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-11-03 22:02:19 +0100

    feat: add entrypoint

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,18 @@
-// TODO
+import { config } from 'dotenv';
+import { IClient, ILogger } from 'src/interfaces';
+import { container } from '../config/ioc.config';
+
+async function bootstrap() {
+  const client = container.get<IClient>('Client');
+  const logger = container.get<ILogger>('Logger');
+
+  // Configure dotenv
+  config();
+
+  // Authenticate with Discord
+  await client.login(process.env.DISCORD_TOKEN);
+
+  logger.info('Server started');
+}
+
+bootstrap();
