e84a036e1dc686d49e85dbe61a09894d63c4b7e4
Sting Alleman <stingalleman@icloud.com>
2020-11-17 20:42:58 +0100

    fix(ban/kick): fix DP

--- src/commands/BanCommand.ts
+++ src/commands/BanCommand.ts
@@ -1,68 +1,69 @@
-import { injectable } from 'inversify';
-import { container } from '../config/ioc.config';
+import { inject, injectable } from 'inversify';
 import {
   ICommand, ICommandArgument, IMessage, ILogger
 } from '../interfaces';
 
-const logger = container.get<ILogger>('Logger');
-
 @injectable()
 export class BanCommand implements ICommand {
+  @inject('logger')
+
   id: string;
 
- args: ICommandArgument[];
+  args: ICommandArgument[];
 
- constructor() {
-   this.id = 'ban';
-   this.args = [{
-     name: 'user',
-     required: true
-   }];
- }
+  logger: ILogger;
 
- async execute(message: IMessage, args: string[]): Promise<void> {
-   // If author does not have the mod role, return
-   if (!message.member.roles.cache.has(process.env.DISCORD_MOD_ROLE)) {
-     await message.channel.send(`<@!${message.author.id}>, you do not have the correct permissions to use this command!`);
-     return;
-   }
+  constructor() {
+    this.id = 'ban';
+    this.args = [{
+      name: 'user',
+      required: true
+    }];
+  }
 
-   // If there are no arguments, return
-   if (!args[0]) {
-     await message.channel.send(`<@!${message.author.id}>, you must provide a user!\nUsage: \`!ban <user>\``);
-     return;
-   }
+  async execute(message: IMessage, args: string[]): Promise<void> {
+    // If author does not have the mod role, return
+    if (!message.member.roles.cache.has(process.env.DISCORD_MOD_ROLE)) {
+      await message.channel.send(`<@!${message.author.id}>, you do not have the correct permissions to use this command!`);
+      return;
+    }
 
-   // Convert mention to ID, and get the user.
-   const user = message.guild.member(args[0].replace(/[!#&<>@\\]/g, ''));
+    // If there are no arguments, return
+    if (!args[0]) {
+      await message.channel.send(`<@!${message.author.id}>, you must provide a user!\nUsage: \`!ban <user>\``);
+      return;
+    }
 
-   // If user does not exist, return
-   if (!user) {
-     await message.channel.send('This user can not be found in this guild.');
-     return;
-   }
+    // Convert mention to ID, and get the user.
+    const user = message.guild.member(args[0].replace(/[!#&<>@\\]/g, ''));
 
-   // If the role of user is above the role of the author, deny.
-   if (message.member.roles.highest.position < user.roles.highest.position) {
-     await message.channel.send('The user you want to ban is above your role!');
-     return;
-   }
+    // If user does not exist, return
+    if (!user) {
+      await message.channel.send('This user can not be found in this guild.');
+      return;
+    }
 
-   try {
-     // Ban the user.
-     //  await user.ban({
-     //    reason: 'Banned by Tribecamp Bot'
-     //  });
-     await message.channel.send(`${user.displayName} got thrown in the void!`);
-     return;
-   } catch (err) {
-     // If bot does not have the correct permissions to ban the specified user, return
-     if (err.message === 'Missing Permissions') {
-       await message.channel.send(`I do not have enough permissions to ban ${user.displayName}!`);
-       return;
-     }
-     await message.channel.send('Unexpected error! Please try again.');
-     logger.error(`Error while trying to ban ${user.displayName}`, err);
-   }
- }
+    // If the role of user is above the role of the author, deny.
+    if (message.member.roles.highest.position < user.roles.highest.position) {
+      await message.channel.send('The user you want to ban is above your role!');
+      return;
+    }
+
+    try {
+      // Ban the user.
+      //  await user.ban({
+      //    reason: 'Banned by Tribecamp Bot'
+      //  });
+      await message.channel.send(`${user.displayName} got thrown in the void!`);
+      return;
+    } catch (err) {
+      // If bot does not have the correct permissions to ban the specified user, return
+      if (err.message === 'Missing Permissions') {
+        await message.channel.send(`I do not have enough permissions to ban ${user.displayName}!`);
+        return;
+      }
+      await message.channel.send('Unexpected error! Please try again.');
+      this.logger.error(`Error while trying to ban ${user.displayName}`, err);
+    }
+  }
 }
--- src/commands/KickCommand.ts
+++ src/commands/KickCommand.ts
@@ -1,65 +1,66 @@
-import { injectable } from 'inversify';
-import { container } from '../config/ioc.config';
+import { inject, injectable } from 'inversify';
 import {
   ICommand, ICommandArgument, IMessage, ILogger
 } from '../interfaces';
 
-const logger = container.get<ILogger>('Logger');
-
 @injectable()
 export class KickCommand implements ICommand {
+  @inject('logger')
+
   id: string;
 
- args: ICommandArgument[];
+  args: ICommandArgument[];
 
- constructor() {
-   this.id = 'kick';
-   this.args = [{
-     name: 'user',
-     required: true
-   }];
- }
+  logger: ILogger
 
- async execute(message: IMessage, args: string[]): Promise<void> {
-   // If author does not have the mod role, return
-   if (!message.member.roles.cache.has(process.env.DISCORD_MOD_ROLE)) {
-     await message.channel.send(`<@!${message.author.id}>, you do not have the correct permissions to use this command!`);
-     return;
-   }
+  constructor() {
+    this.id = 'kick';
+    this.args = [{
+      name: 'user',
+      required: true
+    }];
+  }
 
-   // If there are no arguments, return
-   if (!args[0]) {
-     await message.channel.send(`<@!${message.author.id}>, you must provide a user!\nUsage: \`!kick <user>\``);
-     return;
-   }
-   // Convert mention to ID, and get the user.
-   const user = message.guild.member(args[0].replace(/[!#&<>@\\]/g, ''));
+  async execute(message: IMessage, args: string[]): Promise<void> {
+    // If author does not have the mod role, return
+    if (!message.member.roles.cache.has(process.env.DISCORD_MOD_ROLE)) {
+      await message.channel.send(`<@!${message.author.id}>, you do not have the correct permissions to use this command!`);
+      return;
+    }
 
-   // If user does not exist, return
-   if (!user) {
-     await message.channel.send('This user can not be found in this guild.');
-     return;
-   }
+    // If there are no arguments, return
+    if (!args[0]) {
+      await message.channel.send(`<@!${message.author.id}>, you must provide a user!\nUsage: \`!kick <user>\``);
+      return;
+    }
+    // Convert mention to ID, and get the user.
+    const user = message.guild.member(args[0].replace(/[!#&<>@\\]/g, ''));
 
-   // If the role of user is above the role of the author, deny.
-   if (message.member.roles.highest.position < user.roles.highest.position) {
-     await message.channel.send('The user you want to ban is above your role!');
-     return;
-   }
+    // If user does not exist, return
+    if (!user) {
+      await message.channel.send('This user can not be found in this guild.');
+      return;
+    }
 
-   try {
-     // Kicks the user.
-     await user.kick('Kicked by Tribecamp Bot');
-     await message.channel.send(`${user.displayName} got kicked!`);
-     return;
-   } catch (err) {
-     // If bot does not have the correct permissions to kick the specified user, return
-     if (err.message === 'Missing Permissions') {
-       await message.channel.send(`I do not have enough permissions to kick ${user.displayName}!`);
-       return;
-     }
-     await message.channel.send('Unexpected error! Please try again.');
-     logger.error(`Error while trying to ban ${user.displayName}`, err);
-   }
- }
+    // If the role of user is above the role of the author, deny.
+    if (message.member.roles.highest.position < user.roles.highest.position) {
+      await message.channel.send('The user you want to ban is above your role!');
+      return;
+    }
+
+    try {
+      // Kicks the user.
+      await user.kick('Kicked by Tribecamp Bot');
+      await message.channel.send(`${user.displayName} got kicked!`);
+      return;
+    } catch (err) {
+      // If bot does not have the correct permissions to kick the specified user, return
+      if (err.message === 'Missing Permissions') {
+        await message.channel.send(`I do not have enough permissions to kick ${user.displayName}!`);
+        return;
+      }
+      await message.channel.send('Unexpected error! Please try again.');
+      this.logger.error(`Error while trying to ban ${user.displayName}`, err);
+    }
+  }
 }
