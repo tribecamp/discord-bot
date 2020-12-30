d3bced29067f8d7897c4b854a92fb51ad3a24f2e
Stijn van der Kolk <stijn@kolkies.dev>
2020-12-30 22:08:16 +0100

    chore: fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
   extends: [
     '@tribecamp/base',
     '@tribecamp/typescript'
-  ]
+  ],
+  ignorePatterns: ['.eslintrc.js'],
 };
