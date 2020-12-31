1d854c3de055e9add1f65ae80595d36076221366
Stijn van der Kolk <stijn@kolkies.dev>
2020-12-31 19:35:28 +0100

    fix: fix race condition

--- src/events/JoinEvent.ts
+++ src/events/JoinEvent.ts
@@ -10,12 +10,12 @@ export class JoinEvent implements IEvent {
     this.id = 'guildMemberAdd';
   }
 
-  handler([member]: ClientEvents['guildMemberAdd']): void {
+  async handler([member]: ClientEvents['guildMemberAdd']): Promise<void> {
     if (Date.now() - member.user.createdTimestamp < 1000 * 60 * 60 * 24 * 14) {
       const reason = 'Hey daar! We hebben je helaas van de server moeten verwijderen, omdat je account minder dan 14 dagen oud is. Dit doen wij om spam te voorkomen in de server.\nWe zien je binnenkort graag terug! Excuses voor het ongemak.';
-      member.user.send(reason).catch(() => null);
-      member.kick(reason);
-      (member.guild.channels.resolve(process.env.USER_VALIDATION_CHANNEL) as TextChannel).send(`${member.user.tag} (${member.user.id}) is verwijderd omdat het account te jong was.`);
+      await member.user.send(reason).catch(() => null);
+      await member.kick(reason);
+      await (member.guild.channels.resolve(process.env.USER_VALIDATION_CHANNEL) as TextChannel).send(`${member.user.tag} (${member.user.id}) is verwijderd omdat het account te jong was.`);
       return;
     }
     member.roles.add(process.env.DISCORD_JOIN_ROLE);
