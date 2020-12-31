d9a98956271930edac30b111e51f50665bf4b08b
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-12-31 19:46:47 +0100

    fix(join-event): mixed await/then

--- src/events/JoinEvent.ts
+++ src/events/JoinEvent.ts
@@ -13,11 +13,16 @@ export class JoinEvent implements IEvent {
   async handler([member]: ClientEvents['guildMemberAdd']): Promise<void> {
     if (Date.now() - member.user.createdTimestamp < 1000 * 60 * 60 * 24 * 14) {
       const reason = 'Hey daar! We hebben je helaas van de server moeten verwijderen, omdat je account minder dan 14 dagen oud is. Dit doen wij om spam te voorkomen in de server.\nWe zien je binnenkort graag terug! Excuses voor het ongemak.';
-      await member.user.send(reason).catch(() => null);
+      try {
+        await member.user.send(reason);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error); // TODO: improve error handling
+      }
       await member.kick(reason);
       await (member.guild.channels.resolve(process.env.USER_VALIDATION_CHANNEL) as TextChannel).send(`${member.user.tag} (${member.user.id}) is verwijderd omdat het account te jong was.`);
       return;
     }
-    member.roles.add(process.env.DISCORD_JOIN_ROLE);
+    await member.roles.add(process.env.DISCORD_JOIN_ROLE);
   }
 }
